da6744e710f5151ce7bbacaab3173882c9d2d4e5
Eudo <154047007+Eudolima@users.noreply.github.com>
2026-01-02 21:15:43 +0000

    Lendo arquivo e imprimindo linhas sem vírgulas

--- loop_for.py
+++ loop_for.py
@@ -1,6 +1,6 @@
 with open('treino.txt', 'r', encoding='utf-8') as leitura:
     for linha in leitura:
         linha = linha.replace(',', '')
-        print(linha, end='')
+        print(linha)
 
  
