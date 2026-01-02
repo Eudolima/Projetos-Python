c3a02ce440f40f8f7fb4b576af106c26a1d6b88a
Eudo <154047007+Eudolima@users.noreply.github.com>
2026-01-02 13:45:47 +0000

    interando em lista

--- loop_for.py
+++ loop_for.py
@@ -1,13 +1,6 @@
-class Elemento:
-    def __init__(self, nome, descricao, inscritos):
-        self.nome = nome
-        self.descricao = descricao
-        self.inscritos = inscritos
+lista = ['Alana', 'Rita', 'Eudo']
 
-canal_lancode = Elemento('Lan Code', 'Códigos e gatos', 34000)
-canal_eudo = Elemento('Suporte', 'telefones', 2500)
-
-print(canal_lancode.nome)
-print(canal_eudo.descricao)
+for nome in lista:
+    print(nome)
 
  
