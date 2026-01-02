6f7464b6737fbb3549f04dd4ce5765b53666ab3c
Eudo <154047007+Eudolima@users.noreply.github.com>
2026-01-02 13:34:07 +0000

    Introd a POO

--- loop_for.py
+++ loop_for.py
@@ -1,4 +1,13 @@
-lista = ['Alana', 'Rita', 'Eudo']
+class Elemento:
+    def __init__(self, nome, descricao, inscritos):
+        self.nome = nome
+        self.descricao = descricao
+        self.inscritos = inscritos
 
-for nome in lista:
-    print(nome)
+canal_lancode = Elemento('Lan Code', 'Códigos e gatos', 34000)
+canal_eudo = Elemento('Suporte', 'telefones', 2500)
+
+print(canal_lancode.nome)
+print(canal_eudo.descricao)
+
+ 
