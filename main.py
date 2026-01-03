5e09e7e2b2483995e985f84fa7f75bf586081d7b
Eudo <154047007+Eudolima@users.noreply.github.com>
2026-01-03 20:00:35 +0000

    Webscraping Futebol Inglês

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-def mensagem():
-    return "Olá, Mundo!"
-print(mensagem())
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# 1. O robô visita o site
+url = "https://ge.globo.com/futebol/futebol-internacional/futebol-ingles/"
+resposta = requests.get(url)
+
+
+# 2. O robô lê a página
+site = BeautifulSoup(resposta.text, "html.parser")
+
+
+
+# 3. O robô procura uma tabela (exemplo)
+
+
+# 4. O robô salva no Excel
+
